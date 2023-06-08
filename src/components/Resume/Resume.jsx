da1318ac96910329858b73566f4ab043b5d75d66
spk-krishna567 <20191a0567jntucep@gmail.com>
2023-06-08 20:28:17 +0530

    by uma

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -3,7 +3,23 @@ import React from "react";
 import "./Resume.css";
 
 const Resume = () => {
-  return <div className="Resume" id="resume"></div>;
+  return (
+    <div className="resume" id="resumepage">
+      <div className="resume-heading">
+        <span className="html-entity"> &lt;/&gt;</span> Resume{" "}
+      </div>
+      <div className="resume-subheading">
+        You can download my resume by clicking the below button
+      </div>
+      <div className="button-resume">
+        <a href="https://drive.google.com/file/d/1a00Kz66fJmTKK0cUpLSdrCJiMlXfxoI_/view">
+          <button className="download-resume">
+            <span>Download Resume</span>
+          </button>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
